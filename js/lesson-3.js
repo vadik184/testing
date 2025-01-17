1fc0a1d966a85da138f1c2400bb3fe0ae9ed011b
vadik184 <133372327+vadik184@users.noreply.github.com>
2025-01-17 17:37:59 +0200

    add lesson 3

---
+++ js/lesson-3.js
@@ -0,0 +1,30 @@
+// const arr = [12, 22, 3, 14, 45, 16, 57];
+// const min = Math.min(...arr);
+// console.log(arr); //[12, 22, 3, 14, 45, 16, 57]
+// console.log(min); //3
+
+const user = {
+  user: "test",
+  age: 11,
+  skills: {
+    html: true,
+    css: true,
+    js: false,
+  },
+};
+const {
+  name,
+  skills: { css },
+} = user;
+console.log(css); //true
+
+// const { age: userAge } = user;
+// console.log(age); // помилка
+// console.log(userAge); // 11
+
+// function foo({ name }, car: {bmw = 'none'}={ }) {
+//     console.log(bmw)
+// }
+// foo({ name: 'user1', car: { bmw: 'X5' } })
+// foo({ name: 'user1', car: { audi: 'A6' } })
+// foo({name:'user1'})
--- js/test.js
+++
@@ -1,4 +0,0 @@
-const arr = [12, 22, 3, 14, 45, 16, 57];
-const min = Math.min(...arr);
-console.log(arr); //[12, 22, 3, 14, 45, 16, 57]
-console.log(min); //3
